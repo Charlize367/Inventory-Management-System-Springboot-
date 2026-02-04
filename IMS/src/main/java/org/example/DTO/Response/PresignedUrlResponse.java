898efeab28231b0d60e7bbe11b639ca5bd9c12db
charlizemendoza <charlizecmendoza@gmail.com>
2026-02-04 19:34:37 +0800

    Added workflows, dockerfile, and yaml

---
+++ IMS/src/main/java/org/example/DTO/Response/PresignedUrlResponse.java
@@ -0,0 +1,31 @@
+package org.example.DTO.Response;
+
+
+public class PresignedUrlResponse {
+
+    public PresignedUrlResponse(String uploadUrl, String key) {
+        this.uploadUrl = uploadUrl;
+        this.key = key;
+    }
+
+    public String getUploadUrl() {
+        return uploadUrl;
+    }
+
+    public void setUploadUrl(String uploadUrl) {
+        this.uploadUrl = uploadUrl;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    private String uploadUrl;
+    private String key;
+}
+
+
---
+++ IMS/src/main/java/org/example/Services/S3Service.java
@@ -0,0 +1,54 @@
+package org.example.Services;
+
+import org.example.DTO.Response.PresignedUrlResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+
+public class S3Service {
+
+    public S3Service(S3Presigner s3Presigner) {
+        this.s3Presigner = s3Presigner;
+    }
+
+    private final S3Presigner s3Presigner;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    public PresignedUrlResponse generatePresignedUrl(
+            String filename,
+            String contentType,
+            long fileSize) {
+
+        String key = "uploads/" + UUID.randomUUID() + "/" + filename;
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .contentType(contentType)
+                .contentLength(fileSize)
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(20))
+                .putObjectRequest(putObjectRequest)
+                .build();
+
+        PresignedPutObjectRequest presignedRequest =
+                s3Presigner.presignPutObject(presignRequest);
+
+        return new PresignedUrlResponse(
+                presignedRequest.url().toString(),
+                key
+        );
+    }
+}
