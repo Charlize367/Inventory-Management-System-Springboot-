6f874fa2463721ae1750c71a26b43e8d45e91d88
charlizemendoza <charlizecmendoza@gmail.com>
2026-02-23 19:32:22 +0800

    Updated docker compose yml

--- IMS/src/main/java/org/example/Application.java
+++ IMS/src/main/java/org/example/Application.java
@@ -1,28 +1,16 @@
 package org.example;
 
-import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableCaching
 @EnableAsync
-@RestController
-public abstract class Application  {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-
-    @GetMapping
-    public String HelloWorld(){
-        return "Hello World Spring Boot";
-    }
-
-    public abstract void start(Stage primaryStage);
 }
